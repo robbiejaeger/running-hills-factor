2763e9dae58af8ce2ae96af08614849f53c16e42
Robbie Jaeger <jaeger.rob@gmail.com>
2021-02-28 14:10:32 -0700

    Add library and implementation to parse xml from gpx data

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
+const parseString = require('xml2js').parseString;
 
-fs.readFile('../../../Downloads/test.gpx', 'utf8' , (err, data) => {
+fs.readFile('../../../Downloads/test.gpx', 'utf8' , (err, xml) => {
   if (err) {
     console.error(err);
     return
   }
-  console.log(data);
+
+  parseString(xml, (err, result) => {
+    console.log(result.gpx.rte[0].rtept);
+  });
 })
