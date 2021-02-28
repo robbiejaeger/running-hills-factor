8cc6f27905efcf8ba6b8decc44d97f3af109aa28
Robbie Jaeger <jaeger.rob@gmail.com>
2021-02-28 14:15:15 -0700

    Add error handling for xml parser

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const parseString = require('xml2js').parseString;
 
-fs.readFile('../../../Downloads/test.gpx', 'utf8' , (err, xml) => {
+fs.readFile('../../../Downloads/test.gpx', 'utf8', (err, xml) => {
   if (err) {
     console.error(err);
     return
   }
 
   parseString(xml, (err, result) => {
+    if (err) {
+      console.error(err);
+      return
+    }
+    
     console.log(result.gpx.rte[0].rtept);
   });
 });
